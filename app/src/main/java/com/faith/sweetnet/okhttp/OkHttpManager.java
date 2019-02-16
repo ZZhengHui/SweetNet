4dbbbbee51ec7d07ddaacc1e072bd9a8ff428353
13651654521@163.com <13651654521@163.com>
2019-02-16 18:08:08 +0800

    okhttp基本实现

--- app/src/main/java/com/faith/sweetnet/okhttp/OkHttpManager.java
+++ app/src/main/java/com/faith/sweetnet/okhttp/OkHttpManager.java
@@ -8,8 +8,11 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttpManager {
@@ -18,16 +21,19 @@ public class OkHttpManager {
     }
 
     private static OkHttpManager manager = null;
+    private static OkHttpClient okHttpClient = null;
 
     public static OkHttpManager Builder() {
         if (manager == null) {
             manager = new OkHttpManager();
         }
+        if (okHttpClient == null) {
+            okHttpClient = new OkHttpClient();
+        }
         return manager;
     }
 
     public void get() {
-        OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder().url(SweetUrl.url).get().build();
         okHttpClient.newCall(request)
                 .enqueue(new Callback() {
@@ -45,4 +51,29 @@ public class OkHttpManager {
                 });
     }
 
+    public void post() {
+        MediaType mediaType = MediaType.parse("");
+        Request request = new Request.Builder()
+                .url(SweetUrl.url)
+                .post(RequestBody.create(mediaType, ""))
+                .build();
+        okHttpClient.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d("call ", response.protocol() + " " + response.code() + " " + response.message());
+                        Headers headers = response.headers();
+                        for (int i = 0; i < headers.size(); i++) {
+                            Log.d("call ", headers.name(i) + ":" + headers.value(i));
+                        }
+                        Log.d("call ", "onResponse: " + response.body().string());
+
+                    }
+                });
+    }
 }
